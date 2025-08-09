34f072ae28e0f491faa5a3e98ecc61504eb754c7
Sahil-109 <vaghelasahil26@gmail.com>
2025-08-09 23:01:15 +0530

    commit attractive look changes --SV

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
